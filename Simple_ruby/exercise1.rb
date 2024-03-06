1d6e8b84e02fdd6c4af0f8b0dcd893a46c1e2412
Gracie2024 <kristinegracerodriguez2023@gmail.com>
2024-03-06 10:22:42 +0800

    answer

---
+++ Simple_ruby/exercise1.rb
@@ -0,0 +1,10 @@
+#Write a Ruby program that takes a person's birth year as a variable, calculates their age, and then prints a message with their birth year and current age.
+# Have a variable for the birth year.
+# Calculate the person's age (assuming the current year is 2024).
+# Print a message that includes both the birth year and the calculated age
+def calculate_age(birth_year)
+  current_year = 2024
+  age = birth_year - current_year
+end
+birth_year = 1985
+p = calculate_age(birth_year)
