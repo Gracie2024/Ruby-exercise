defdf28fa0411167cd060047b6606ce5a71d8ae6
Gracie2024 <kristinegracerodriguez2023@gmail.com>
2024-03-06 14:18:18 +0800

    exercise

--- Simple_ruby/exercise_2.rb
+++ Simple_ruby/exercise_2.rb
@@ -2,4 +2,14 @@
 # Have variables for two numbers.
 # Perform addition, subtraction, multiplication, and division on the two numbers.
 # Print a message that includes the original numbers and the results of each operation.
-#
+print 'input a number: '
+a = gets.chomp.to_i
+print 'input a number: '
+b = gets.chomp.to_i
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b.to_f
+operation = ["#{sum}","#{difference}","#{product}","#{quotient}"]
+
+puts operation
