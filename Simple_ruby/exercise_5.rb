552c448b40e32b9a3cf7cf7ec821b0c5349faf30
Gracie2024 <kristinegracerodriguez2023@gmail.com>
2024-03-07 15:45:06 +0800

    calculates the value of area

--- Simple_ruby/exercise_5.rb
+++ Simple_ruby/exercise_5.rb
@@ -1,10 +1,9 @@
 # Write a Ruby program that takes the length and width of a rectangle as variables, calculates the area, and prints the result.
 
-print 'input a number:'
+print 'input a length:'
 length = gets.chomp.to_i
-print 'input a number:'
+print 'input a width:'
 width = gets.chomp.to_i
 area = length * width
 
 puts "the result of area is #{area}"
-#
