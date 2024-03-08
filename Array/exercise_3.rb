3e023a9fc12469ff885edb903aa1610eb1c7e2fe
Gracie2024 <kristinegracerodriguez2023@gmail.com>
2024-03-08 16:05:22 +0800

    Determining the age group for Adults,Teenager and child

--- Array/exercise_3.rb
+++ Array/exercise_3.rb
@@ -1,2 +1,20 @@
-v=1
-p v
+# Develop a Ruby program that receives an array of ages
+# and counts how many individuals fall into each age group: "Child" (12 or younger),
+# "Teenager" (13 to 19), and "Adult" (20 or older).
+
+individuals = [20, 10, 8, 15, 5]
+
+group = []
+
+individuals.map do |age|
+  if age >= 20 && age <=30
+    group << 'Adult'
+  elsif age >13 && age <=19
+    group << 'Teenager'
+  else age > 12
+  group << 'Child'
+
+  end
+end
+
+p group
