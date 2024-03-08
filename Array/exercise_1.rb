11b3b9e740caf9529ea7533d405d80c7bea8c468
Gracie2024 <kristinegracerodriguez2023@gmail.com>
2024-03-08 14:07:43 +0800

    Classify and store student's score

--- Array/exercise_1.rb
+++ Array/exercise_1.rb
@@ -4,31 +4,37 @@
 # and "D" for scores below 70.
 
 def score_a?(score)
-  score>= 90 && score<=99
+  score >= 90 && score <= 99
 end
-student_scores=[]
+
+student_scores = []
+
 while true
-  print'(press x to stop inputting score)Input student score
+  print '(press x to stop inputting score)Input student score: '
   input = gets.chomp
 
-if input=='x'
- break
-else
- student_scores<<input.to.i
-end
+  if input == 'x'
+    break
+  else
+    student_scores << input.to_i
+  end
 end
 
-#student_scores= [85,92,75,60,78,90,99,100]
+#student_scores = [85, 92, 75, 60, 78, 90, 99, 100]
 
-new_student_scores= student_scores.map do |score|
-if score==100
-"A+"
-elsif score_a?(score)
-"A"
-elsif score>=80  && score<=89
-"B"
-elsif score>=70  && score<=79
-"C"
-elsif score<70
-"D"
+new_student_scores = student_scores.map do |score|
+  if score == 100
+    "A+"
+  elsif score_a?(score)
+    "A"
+  elsif score >= 80 && score <= 89
+    "B"
+  elsif score >= 70 && score <= 79
+    "C"
+  elsif score < 70
+    "D"
+  end
 end
+
+
+p new_student_scores
